6313373d5d13703f4366ed79cf9ee9250188ba39
Alexsandr <venoman9867@gmail.com>
2023-02-18 17:44:40 +0300

    commit

--- src/homework_2/task_1/java/StringBuilderSQL.java
+++ src/homework_2/task_1/java/StringBuilderSQL.java
@@ -1,14 +1,14 @@
 public class StringBuilderSQL {
+    //Программа должна составлять SQL запрос по JSON строке
+    //{"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
     public static void main(String[] args) {
-        String name="Ivan";
-        String country="Russia";
-        String city="Moscow";
-        String age="null";
-        System.out.println(makeString(name,country,city,age));
+        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        System.out.println(makeString(json));
     }
-    public static String makeString(String name, String country, String city, String age){
-        StringBuilder stringBuilder=new StringBuilder();
-        stringBuilder.append(name).append(" ").append(country).append(" ").append(city).append(" ").append(age);
-        return stringBuilder.toString().replaceAll("null","");
+
+    public static String makeString(String json) {
+        StringBuilder stringBuilder = new StringBuilder("select * from students where ");
+        stringBuilder.append(json);
+        return stringBuilder.toString().replaceAll("\"+[A-Za-z]+\":\"null\"" ,"");//Костыль XD
     }
 }
