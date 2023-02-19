f92e6e365556a57d850e9a8721e1796c549118c6
Alexsandr <venoman9867@gmail.com>
2023-02-19 21:18:05 +0300

    commit

--- src/homework_2/task_1/java/StringBuilderSQL.java
+++ src/homework_2/task_1/java/StringBuilderSQL.java
@@ -1,14 +1,39 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class StringBuilderSQL {
-    //Программа должна составлять SQL запрос по JSON строке
-    //{"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
-    public static void main(String[] args) {
-        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+    public static void main(String[] args) throws JSONException {
+        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"null\", \"age\":\"24\"}";
         System.out.println(makeString(json));
     }
 
-    public static String makeString(String json) {
+    public static String makeString(String json) throws JSONException {
         StringBuilder stringBuilder = new StringBuilder("select * from students where ");
-        stringBuilder.append(json);
-        return stringBuilder.toString().replaceAll("[-,\s]?\s?\"[A-Za-z]+\":\"null\"[-,\s]?\s?" ,"");
+        JSONObject jsonObject = new JSONObject(json);
+        boolean flag = false;
+        if (jsonObject.get("name").hashCode() != "null".hashCode()) {
+            stringBuilder.append(jsonObject.get("name"));
+            flag = true;
+        }
+        if (jsonObject.get("country").hashCode() != "null".hashCode()) {
+            if (flag == true) {
+                stringBuilder.append(" and ");
+            }
+            stringBuilder.append(jsonObject.get("country"));
+        }
+
+        if (jsonObject.get("city").hashCode() != "null".hashCode()) {
+            if (flag == true) {
+                stringBuilder.append(" and ");
+            }
+            stringBuilder.append(jsonObject.get("city"));
+        }
+        if (jsonObject.get("age").hashCode() != "null".hashCode()) {
+            if (flag == true) {
+                stringBuilder.append(" and ");
+            }
+            stringBuilder.append(jsonObject.get("age"));
+        }
+        return stringBuilder.toString();
     }
 }
